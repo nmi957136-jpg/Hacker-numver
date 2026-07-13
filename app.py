d4d8d3297f39131c75430f29232f077b130af453
nmi957136-jpg <nmi957136@gmail.com>
2026-07-13 17:36:14 +0900

    Add initialization sequence to app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import time
+
+lines = [
+    "Initializing...",
+    "Loading modules...",
+    "Connecting...",
+    "Access granted.",
+    "System online."
+]
+
+for line in lines:
+    print(line)
+    time.sleep(0.5)
+
+print("\n> root@localhost:~$")
